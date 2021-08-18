cb628722725b79404e683132c4feebb63ed09f44
Marco Schumacher <marco@schumacher.dev>
2021-08-18 09:34:30 +0200

    Longer timeout in tests.

--- test/_helpers.ts
+++ test/_helpers.ts
@@ -1,6 +1,6 @@
 import { sleep } from '../src/helpers';
 
-export const poll = async (condition: () => unknown, timeout = 1000, interval = 10): Promise<void> => {
+export const poll = async (condition: () => unknown, timeout = 10000, interval = 10): Promise<void> => {
   for (let t = 0; t < timeout; t += interval) {
     await sleep(interval);
     if (condition()) return;
