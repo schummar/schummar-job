a89ad540622a70c6cd2c6e78c6789cc7ed9c7223
Marco Schumacher <marco@schumacher.dev>
2021-08-19 08:43:30 +0200

    fix: schedule new tasks only in the future.

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,21 +6,23 @@ export type MaybePromise<T> = T | Promise<T>;
 export const sleep = (ms: number): Promise<void> => new Promise((r) => setTimeout(r, ms));
 
 export const calcNextRun = (schedule: Schedule, lastRun = new Date()): Date => {
+  const t = Math.max(lastRun.getTime(), Date.now());
+
   if ('milliseconds' in schedule) {
-    return new Date(lastRun.getTime() + schedule.milliseconds);
+    return new Date(t + schedule.milliseconds);
   }
   if ('seconds' in schedule) {
-    return new Date(lastRun.getTime() + schedule.seconds * 1000);
+    return new Date(t + schedule.seconds * 1000);
   }
   if ('minutes' in schedule) {
-    return new Date(lastRun.getTime() + schedule.minutes * 60 * 1000);
+    return new Date(t + schedule.minutes * 60 * 1000);
   }
   if ('hours' in schedule) {
-    return new Date(lastRun.getTime() + schedule.hours * 60 * 60 * 1000);
+    return new Date(t + schedule.hours * 60 * 60 * 1000);
   }
   if ('days' in schedule) {
-    return new Date(lastRun.getTime() + schedule.days * 24 * 60 * 60 * 1000);
+    return new Date(t + schedule.days * 24 * 60 * 60 * 1000);
   }
 
-  return parseCronExpression(schedule.cron).getNextDate(lastRun);
+  return parseCronExpression(schedule.cron).getNextDate(new Date(t));
 };
